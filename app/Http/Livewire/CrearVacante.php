a4e92fad850ed7729a9a463a9b24100188d7a852
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-10-01 19:06:41 +0200

    Formulario para vacantes

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Categoria;
+use App\Models\Salario;
 use Livewire\Component;
 
 class CrearVacante extends Component
 {
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required'
+    ];
+
     public function render()
     {
-        return view('livewire.crear-vacante');
+        // Consultamos la base de datos
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
+
+
+        return view('livewire.crear-vacante', [
+            "salarios" => $salarios,
+            'categorias' => $categorias
+        ]);
     }
 }
