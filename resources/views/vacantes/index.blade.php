98da3ae238be65fc44736a00248babfd6b081884
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-10-03 19:21:56 +0200

    paginacion y mostrar listado vacantes

--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -12,13 +12,8 @@
                     {{ session('mensaje') }}
                 </div>
             @endif
+            <livewire:mostrar-vacantes />
 
-
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Mis Vacantes
-                </div>
-            </div>
         </div>
     </div>
 </x-app-layout>
