ea503ac2f6b3ae4d4d861ea97ae4a1eacdbd0787
PilarFernandezNieto <apreilmc@gmail.com>
2024-10-06 13:03:46 +0200

    Mostrando candidatos

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -9,7 +9,7 @@
                     <p class="text-sm text-gray-500">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
                 </div>
                 <div class="flex flex-col md:flex-row items-stretch gap-3  mt-5 md:mt-0">
-                    <a href=""
+                    <a href="{{route('candidatos.index', $vacante->id)}}"
                         class="text-center bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase">Candidatos</a>
                     <a href="{{ route('vacantes.edit', $vacante->id) }}"
                         class="text-center bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase">Editar</a>
